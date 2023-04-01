2dcc0ced05d24bf1bb43392fe1716cdfeea3d9f5
arxisme <arsh11100arsh@gmail.com>
2023-04-01 00:45:19 +0000

    hello

---
+++ hackvita/my-react-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <div class="top-bar">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+      <div class="content">
+        <div class="col-1">JJ</div>
+        <div class="col-2"></div>
+        <div class="col-3"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
